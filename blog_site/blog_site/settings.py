8cea56a143f4882c1b56b78608bc898cfcdbcc55
kyrylokireiev <kyrylo.kireiev@yalantis.net>
2022-11-25 17:18:58 +0200

    connect postgres db, fix data migrations

--- blog_site/blog_site/settings.py
+++ blog_site/blog_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -85,11 +86,22 @@ WSGI_APPLICATION = 'blog_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': 'test_base',
+      'USER': 'postgres',
+      'PASSWORD': '1358',
+      'HOST': 'localhost',
+      'PORT': '5432',
+   }
 }
 
 
